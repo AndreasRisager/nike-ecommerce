2e309c42467f6bc5afdc32beac069d5864df09a4
Andreas Risager <andreasbarkov@gmail.com>
2021-06-02 11:42:47 +0200

    first commit

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6bbec5a6425562a6b7be11407435330e'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ebffc730-5c0f-4d2a-8e1f-7380e1feb0c5'
+};
